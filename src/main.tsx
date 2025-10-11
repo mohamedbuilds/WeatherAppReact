0eb849e22ed0d821c76e494ac1b06fe31e2ebb07
Mohamed Hosny <mamedhosny345@gmail.com>
2025-10-11 18:54:08 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../../weather-app/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 import App from './App.tsx'
 import { register } from './serviceWorker.ts'
