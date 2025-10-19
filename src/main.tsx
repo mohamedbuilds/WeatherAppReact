b42befe58241b42c1b42078af64ae449616007be
Mohamed Hosny <mamedhosny345@gmail.com>
2025-10-19 14:59:42 +0300

    Fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 import App from './App.tsx'
-import { register } from './serviceWorker.ts'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-register();
